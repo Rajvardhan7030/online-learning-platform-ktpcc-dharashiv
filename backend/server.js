1f86a52c6aa42670382ec3104b61afe22628eb6a
Rajvardhan7030 <rajvardhanjadhav7030@gmail.com>
2026-05-17 01:58:48 +0530

    fixrd cors esssue

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,10 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://127.0.0.1:3000',
     'http://localhost:5500',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://e-learn.in',
+    'https://www.e-learn.in',
+    'https://ide.e-learn.in'
 ];
 
 const corsOptions = {
